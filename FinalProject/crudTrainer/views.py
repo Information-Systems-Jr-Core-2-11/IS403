2b32e0de837bb4a3d94a7a2c7ee41be670d9dac9
Jonah Duffin <jonahfduffin@gmail.com>
2020-11-04 10:21:41 -0700

    fixed httResponse to HttpResponse

--- FinalProject/crudTrainer/views.py
+++ FinalProject/crudTrainer/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttResponse
+from django.http import HttpResponse
 
 def crudTrainerPageView(request) :
     return HttpResponse('crudTrainer')
