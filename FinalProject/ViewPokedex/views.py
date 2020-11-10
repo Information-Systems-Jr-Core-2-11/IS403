62855530b142d592a4c9fb8c1b6e41b5c4448aeb
Jonah Duffin <jonahfduffin@gmail.com>
2020-11-10 09:17:52 -0700

    Jonah's edits following class in ViewPokedex

--- FinalProject/ViewPokedex/views.py
+++ FinalProject/ViewPokedex/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def pokedexPageView(request):
+    context = {
+        "name" : "jonah",
+        "movies" : ["Star wars", "Three Amigos", "Elf"]
+    }
     # return render(request, './template/index.html')
-    return HttpResponse('Pokedex')
+    return render(request, 'ViewPokedex/index.html')
