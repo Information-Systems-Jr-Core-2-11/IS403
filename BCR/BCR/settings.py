434a9037075fc310556b4cd1877e59fe4be623e0
Jonah Duffin <jonahfduffin@gmail.com>
2020-12-07 09:44:48 -0700

    added database compatibility

--- BCR/BCR/settings.py
+++ BCR/BCR/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'applicant.apps.ApplicantConfig',
+    'employer.apps.EmployerConfig',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'BCR.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+                'ENGINE': 'django.db.backends.postgresql',
+                'NAME': 'bcr',
+                'USER': 'postgres',
+                'PASSWORD': 'password',
+                'HOST': 'localhost'
     }
 }
 
